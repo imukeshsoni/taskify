3a3cc5ec6bfda0572514174e0e5f2daba72d33d0
Mukesh Soni <msony552@gmail.com>
2022-05-06 17:57:27 +0530

    added todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/inputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+    setTodo("");
+  }
+
+  console.log(todos);
+  
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div> 
   );
 }
---
+++ src/model.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+    id: number | string;
+    todo: string;
+    isDone: boolean
+}
