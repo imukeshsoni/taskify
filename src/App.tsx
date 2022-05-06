7996bda8fe5409e29764e6c1b355b8d443e82a3d
Mukesh Soni <msony552@gmail.com>
2022-05-06 19:33:04 +0530

    added single todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/inputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 const App: React.FC = () => {
@@ -13,13 +14,11 @@ const App: React.FC = () => {
     setTodo("");
   }
 
-  console.log(todos);
-  
-
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div> 
   );
 }
